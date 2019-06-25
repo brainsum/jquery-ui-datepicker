70e7a26fc237aa5dc7e1c00f9abb9aa69adfe47f
mhavelant <mhavelant@brainsum.com>
2019-06-25 12:47:47 +0200

    Travis updates.

--- .travis-ci/site-install.sh
+++ .travis-ci/site-install.sh
@@ -13,20 +13,11 @@ composer config extra.patches-file "composer.patches.json" \
   && composer config extra.merge-plugin.merge-dev true \
   && composer config extra.merge-plugin.merge-extra true \
   && composer config discard-changes true \
-  && composer require brainsum/tieto_modules:master \
+  && composer require brainsum/jquery_ui_datepicker:master \
   && composer config extra.merge-plugin.include "{{ include_placeholder }}" \
-  && sed -i 's/\"{{ include_placeholder }}\"/\[\"web\/modules\/contrib\/tieto_modules\/*\/composer.json\"\]/' composer.json \
+  && sed -i 's/\"{{ include_placeholder }}\"/\[\"web\/modules\/contrib\/jquery_ui_datepicker\/composer.json\"\]/' composer.json \
   && composer update --lock \
   && cd web \
   && drush site-install --site-name="Test" --account-pass=123 --db-url=mysql://drupal:drupal@mariadb/drupal standard -y \
-  && drush en \
-    jquery_ui_datepicker \
-    tieto_admin_pages \
-    tieto_general_ui \
-    tieto_ldap \
-    tieto_link_customization \
-    tieto_linkit \
-    tieto_tibr \
-    tieto_unpublish_ui \
-    tieto_wysiwyg -y \
+  && drush en jquery_ui_datepicker -y \
   && drush cr
